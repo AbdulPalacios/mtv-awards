caeb18e9aab0e017a2c0c07eb37c3ee41f1f4133
JesLol <juanesmas50@gmail.com>
2025-11-30 15:57:23 -0600

    Se agregan las imagenes de fondo al index

--- index.php
+++ index.php
@@ -9,12 +9,13 @@
     <link rel="stylesheet" href="<?php echo $host?>recursos/assets/css/index.css">
 </head>
 <body>
-    <div>
+    <div class="header-container">
         <?php
             include('recursos/recursos_portal/header.php');
         ?>
     </div>
     <main class="main">
+        <img src="<?php echo $host?>recursos/assets/img/hero__mtv-awards.png" alt="">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, dignissimos asperiores temporibus distinctio labore voluptates nobis quia ipsa! Quo similique inventore est neque labore corporis facere voluptate exercitationem hic at!
     </main>
     <footer>
