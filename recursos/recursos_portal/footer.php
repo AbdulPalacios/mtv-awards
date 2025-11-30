53fdc55ba931d1c4fa5a3a0f7462206106726a05
APJ-VIRGO <kiuvy15master@gmail.com>
2025-11-29 23:21:36 -0600

    actualizacion de ramificacion y agregacion de BD imagenes,actualizacion de variables y un reset.css

---
+++ recursos/recursos_portal/footer.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+    <link rel="stylesheet" href="../assets/css/reset.css">
+    <link rel="stylesheet" href="../assets/css/root.css">
+    <link rel="stylesheet" href="../assets/css/footer.css">
+
+    
+    <title>Pie de página MTV</title>
+</head>
+<body>
+
+    <footer class="footer">
+        <div class="footer__container">
+            <ul>
+                <li><a href="https://viacom.helpshift.com/hc/en/18-mtv/">preguntas frecuentes/ayuda</a></li>
+                <li><a href="https://www.mtv.com/legal/o3ccr8/terms-of-use">condiciones de uso</a></li>
+                <li><a href="https://privacy.paramount.com/en/policy?r=www.mtv.com">políticas de privacidad</a></li>
+                <li><a href="https://www.mtv.com/legal/xuqfse/copyright-compliance">copyright</a></li>
+                <li><a href="http://www.tvguidelines.org/ratings.html">calificaciones de tv</a></li>
+                <li><a href="https://careers.paramount.com/">carreras</a></li>
+                <li><a href="https://www.keepparamount.com/">mantén paramount</a></li>
+            </ul>
+        </div>
+
+        <div class="footer__links-social">
+            <ul>
+                <li>
+                    <a href="https://www.instagram.com/mtv/" target="_blank" rel="noopener noreferrer" aria-label="Instagram">
+                        <i class="fa-brands fa-instagram"></i>
+                    </a>
+                </li>
+                <li>
+                    <a href="http://www.facebook.com/MTV" target="_blank" rel="noopener noreferrer" aria-label="Facebook">
+                        <i class="fa-brands fa-square-facebook"></i>
+                    </a>
+                </li>
+                <li>
+                    <a href="https://twitter.com/MTV" target="_blank" rel="noopener noreferrer" aria-label="Twitter X">
+                        <i class="fa-brands fa-x-twitter"></i>
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.tiktok.com/@mtv?lang=en" target="_blank" rel="noopener noreferrer" aria-label="TikTok">
+                        <i class="fa-brands fa-tiktok"></i>
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.youtube.com/user/MTV" target="_blank" rel="noopener noreferrer" aria-label="YouTube">
+                        <i class="fa-brands fa-youtube"></i>
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.threads.net/@mtv" target="_blank" rel="noopener noreferrer" aria-label="Threads">
+                        <i class="fa-brands fa-threads"></i>
+                    </a>
+                </li>
+            </ul>
+        </div>
+
+        <div class="footer__bottom">
+            <p>
+                © 2025 Viacom International Inc. Todos los derechos reservados. MTV, EMA y todos los títulos, logotipos y personajes relacionados son marcas comerciales de Viacom International Inc.
+            </p>
+        </div>
+    </footer>
+
+    <script src="https://kit.fontawesome.com/e2dc84faef.js" crossorigin="anonymous"></script>
+</body>
+</html>
