72aad1506620e505a420b1beb8ce1e29a07d03b1
APJ-VIRGO <kiuvy15master@gmail.com>
2025-11-30 22:39:28 -0600

    actualizando estilos del index y borrando archovos obsoletos

--- index.php
+++ index.php
@@ -5,8 +5,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <title>MTV Video Music Awards 2025</title>
+
+    <link rel="stylesheet" href="<?php echo $host?>recursos/assets/css/reset.css">
+    <link rel="stylesheet" href="<?php echo $host?>recursos/assets/css/root.css">
+    <link rel="stylesheet" href="<?php echo $host?>recursos/assets/css/header.css">
     <link rel="stylesheet" href="<?php echo $host?>recursos/assets/css/index.css">
+    <link rel="stylesheet" href="<?php echo $host?>recursos/assets/css/footer.css">
 </head>
 <body>
     <div class="header-container">
@@ -14,10 +20,44 @@
             include('recursos/recursos_portal/header.php');
         ?>
     </div>
-    <main class="main">
-        <img src="<?php echo $host?>recursos/assets/img/hero__mtv-awards.png" alt="">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, dignissimos asperiores temporibus distinctio labore voluptates nobis quia ipsa! Quo similique inventore est neque labore corporis facere voluptate exercitationem hic at!
-    </main>
+
+    <div class="hero">
+        <div class="hero__container">
+            <h1>MTV VMAs 2025</h1>
+            <span>
+                Decide cuál de tus celebridades favoritas ganara a lo grande y se llevara a casa una Moon Person en el show de este año.
+            </span>
+        </div>
+    </div>
+    
+    <div class="bg-image">
+        <main class="main">
+            <div class="mtv-music-container">
+                <h3>MTV Video Music Awards</h3>
+                <p>Los MTV Video Music Awards están dedicados a celebrar a los artistas y videos musicales más importantes del año con actuaciones, honores y más.</p>
+                <p>Somos el puente directo entre los artistas y sus seguidores. A través de este sistema integral de votación, gestionamos millones de voces alrededor del mundo para garantizar que cada voto cuente de manera transparente y segura.</p>
+                <ul>
+                    <li>
+                        <a href="https://www.facebook.com/VMAs/">
+                            <i class="fa-brands fa-square-facebook"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.instagram.com/vmas/">
+                            <i class="fa-brands fa-instagram"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://x.com/vmas">
+                            <i class="fa-brands fa-x-twitter"></i>
+                        </a>
+                    
+                    </li>
+                </ul>
+            </div>
+        </main>
+    </div>
+
     <footer>
         <?php
             include('recursos/recursos_portal/footer.php');
