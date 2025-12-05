ab840986dd559d0bdab5889d3f7944e0fc57383a
APJ-VIRGO <kiuvy15master@gmail.com>
2025-12-05 16:57:45 -0600

    terminado de diseño login.php

--- app/views/portal/login.php
+++ app/views/portal/login.php
@@ -9,29 +9,46 @@ require_once '../../../config/constantes.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login - MTV Awards</title>
+    <link rel="stylesheet" href="<?php echo HOST; ?>recursos/assets/css/reset.css">
     <link rel="stylesheet" href="<?php echo HOST; ?>recursos/assets/css/root.css">
-    <link rel="stylesheet" href="<?php echo HOST; ?>recursos/assets/css/index.css">
+    <link rel="stylesheet" href="<?php echo HOST; ?>recursos/assets/css/header.css">
+    <link rel="stylesheet" href="<?php echo HOST; ?>recursos/assets/css/login.css">
+    <link rel="stylesheet" href="<?php echo HOST; ?>recursos/assets/css/footer.css">
 </head>
 <body>
 
-    <?php include '../../../recursos/recursos_portal/header.php'; ?>
+    <div class="bg-image-login">
+        <?php include '../../../recursos/recursos_portal/header.php'; ?>
 
-    <main class="container">
-        <h2>Iniciar Sesión</h2>
-        <form action="../../backend/portal/login.php" method="POST">
+        <main class="container">
+            <h2><i class="fa-solid fa-user-astronaut"></i> Iniciar Sesión</h2>
             
-            <label for="email">Correo Electrónico:</label>
-            <input type="email" name="email" required>
+            <form action="../../backend/portal/login.php" method="POST">
+                
+                <label for="email">Correo Electrónico:</label>
+                <div class="input-group">
+                    <i class="fa-solid fa-envelope icon"></i>
+                    <input type="email" name="email" placeholder="ejemplo@email.com" required>
+                </div>
 
-            <label for="password">Contraseña:</label>
-            <input type="password" name="password" required>
+                <label for="password">Contraseña:</label>
+                <div class="input-group">
+                    <i class="fa-solid fa-lock icon"></i>
+                    <input type="password" name="password" placeholder="••••••••" required>
+                </div>
 
-            <button type="submit">Entrar</button>
-        </form>
-        <p>¿No tienes cuenta? <a href="registrarse.php">Regístrate aquí</a></p>
-    </main>
+                <button type="submit">
+                    Entrar <i class="fa-solid fa-arrow-right-to-bracket"></i>
+                </button>
+
+            </form>
+            
+            <p>¿No tienes cuenta? <a href="registrarse.php">Regístrate aquí</a></p>
+        </main>
+    </div>
 
     <?php include '../../../recursos/recursos_portal/footer.php'; ?>
 
+    <script src="https://kit.fontawesome.com/e2dc84faef.js" crossorigin="anonymous"></script>
 </body>
 </html>
